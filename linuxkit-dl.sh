f14f1d4d647493d81b4450f93d3fcafaf05a28d5
eknath <ek-nath@users.noreply.github.com>
2022-10-31 15:09:09 -0700

    Update linuxkit-dl.sh

--- linuxkit-dl.sh
+++ linuxkit-dl.sh
@@ -2,7 +2,7 @@
 cd /usr/src
 
 echo "downloading"
-wget -O $(uname -r).tar.gz https://mirrors.aliyun.com/linux-kernel/v5.x/linux-5.10.76.tar.gz
+wget -O $(uname -r).tar.gz https://mirrors.aliyun.com/linux-kernel/v5.x/linux-5.15.49.tar.gz
 tar -zxvf $(uname -r).tar.gz -C .
 mv linux-5.10.76/ $(uname -r)
 
