4b3a1a7b9b8a4eb0a1073705c2eeb4a6649021a1
eknath <ek-nath@users.noreply.github.com>
2022-10-31 15:09:23 -0700

    Update linuxkit-dl.sh

--- linuxkit-dl.sh
+++ linuxkit-dl.sh
@@ -4,7 +4,7 @@ cd /usr/src
 echo "downloading"
 wget -O $(uname -r).tar.gz https://mirrors.aliyun.com/linux-kernel/v5.x/linux-5.15.49.tar.gz
 tar -zxvf $(uname -r).tar.gz -C .
-mv linux-5.10.76/ $(uname -r)
+mv linux-5.15.49/ $(uname -r)
 
 echo "creating dir"
 mkdir -p /lib/modules/$(uname -r)
